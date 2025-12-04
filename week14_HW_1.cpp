0ade6c7da332572481e740e7c54a936492c52637
SeoooooooooooHyun <164285144+Seohyun0316@users.noreply.github.com>
2025-12-04 14:15:47 +0900

    edit
    Updated the program to format output as a table and improved file writing.

--- week14_HW_1.cpp
+++ week14_HW_1.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -14,6 +15,7 @@ int main() {
     }
 
     while (true) {
+        cout << "<person information>" << endl;
         cout << "name: ";
         cin >> name;
         if (name == "0") break;  // 이름에 "0" 입력 시 종료
@@ -24,26 +26,33 @@ int main() {
         cin >> phone_number;
 
         // 엑셀 파일에 입력 정보 저장
-        fout << "<person information>\n";
-        fout << "name: " << name << "\n";
-        fout << "address: " << address << "\n";
-        fout << "phone number: " << phone_number << "\n";
-        fout << "<person information>\n";
+        fout << name << "," << address << "," << phone_number << "\n";
     }
 
     fout.close();  // 파일 닫기
 
-    // 파일을 다시 읽기 모드로 열어 저장된 내용 출력
+    // 파일을 다시 읽기 모드로 열어 저장된 내용 표로 출력
     ifstream fin("phonebook.xls");
     if (!fin) {
         cout << "파일 열기 실패!" << endl;
         return 1;
     }
 
-    cout << "\n저장된 내용은 다음과 같습니다:\n";
+    // 표 헤더 출력
+    cout << left << setw(20) << "Name" << setw(30) << "Address" << setw(15) << "Phone Number" << endl;
+
     string line;
     while (getline(fin, line)) {
-        cout << line << endl;
+        size_t pos1 = line.find(',');
+        size_t pos2 = line.rfind(',');
+
+        if (pos1 != string::npos && pos2 != string::npos && pos1 != pos2) {
+            string name = line.substr(0, pos1);
+            string address = line.substr(pos1 + 1, pos2 - pos1 - 1);
+            string phone = line.substr(pos2 + 1);
+
+            cout << left << setw(20) << name << setw(30) << address << setw(15) << phone << endl;
+        }
     }
 
     fin.close();  // 파일 닫기
